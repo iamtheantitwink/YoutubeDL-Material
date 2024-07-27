d5dee34c986a5997cfa4390c05bf84690daf6b69
David Gerard <me@iamtheantitwink.com>
2024-07-26 23:55:12 -0400

    change fetch-downloader & ffmpeg-fetcher to store the binaries in /home/site/wwwroot

--- docker-utils/fetch-twitchdownloader.sh
+++ docker-utils/fetch-twitchdownloader.sh
@@ -5,19 +5,25 @@
 # xoxo :D
 
 case $(uname -m) in
-  x86_64)
-    ARCH=Linux-x64;;
-  aarch64)
-    ARCH=LinuxArm64;;
-  armhf)
-    ARCH=LinuxArm;;
-  armv7)
-    ARCH=LinuxArm;;
-  armv7l)
-    ARCH=LinuxArm;;
-  *)
-    echo "Unsupported architecture: $(uname -m)"
-    exit 1
+x86_64)
+  ARCH=Linux-x64
+  ;;
+aarch64)
+  ARCH=LinuxArm64
+  ;;
+armhf)
+  ARCH=LinuxArm
+  ;;
+armv7)
+  ARCH=LinuxArm
+  ;;
+armv7l)
+  ARCH=LinuxArm
+  ;;
+*)
+  echo "Unsupported architecture: $(uname -m)"
+  exit 1
+  ;;
 esac
 
 echo "(INFO) Architecture detected: $ARCH"
@@ -26,14 +32,14 @@ apt-get update && apt-get -y install unzip curl jq libicu70
 VERSION=$(curl --silent "https://api.github.com/repos/lay295/TwitchDownloader/releases" | jq -r --arg arch "$ARCH" '[.[] | select(.assets | length > 0) | select(.assets[].name | contains("CLI") and contains($arch))] | max_by(.published_at) | .tag_name')
 echo "(2/5) DOWNLOAD - Acquire twitchdownloader"
 curl -o twitchdownloader.zip \
-    --connect-timeout 5 \
-    --max-time 120 \
-    --retry 5 \
-    --retry-delay 0 \
-    --retry-max-time 40 \
-    -L "https://github.com/lay295/TwitchDownloader/releases/download/$VERSION/TwitchDownloaderCLI-$VERSION-$ARCH.zip"
+  --connect-timeout 5 \
+  --max-time 120 \
+  --retry 5 \
+  --retry-delay 0 \
+  --retry-max-time 40 \
+  -L "https://github.com/lay295/TwitchDownloader/releases/download/$VERSION/TwitchDownloaderCLI-$VERSION-$ARCH.zip"
 unzip twitchdownloader.zip
 chmod +x TwitchDownloaderCLI
 echo "(3/5) Smoke test"
 ./TwitchDownloaderCLI --help
-cp ./TwitchDownloaderCLI /usr/local/bin/TwitchDownloaderCLI
+cp ./TwitchDownloaderCLI ./bin/TwitchDownloaderCLI
